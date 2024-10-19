46e7383991cd7c49d14d7e8642ae08a3d7def609
MxN6 <erbolulim2006@gmail.com>
2024-10-19 13:19:59 +0500

    small update hash and style

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Course from './pages/Course';
 import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/course/:id" component={Course} />
-        <Route path="/profile" component={Profile} />
+        <Route exact path="/" component={Home} />
+        <Route path="#/login" component={Login} />
+        <Route path="#/register" component={Register} />
+        <Route path="#/dashboard" component={Dashboard} />
+        <Route path="#/course" component={Course} />
+        <Route path="#/profile" component={Profile} />
       </Switch>
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
