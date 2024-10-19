f8580a9c6d0045215b5c0892f2b3fca8a08ba065
MxN6 <erbolulim2006@gmail.com>
2024-10-19 13:50:39 +0500

    body min height change

--- lms/src/pages/Login.js
+++ lms/src/pages/Login.js
@@ -1,16 +1,42 @@
-import React from 'react';
+// Внутри вашего компонента, например, в Login.js
+import React, { useEffect } from 'react';
 
 const Login = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  useEffect(() => {
+    const telegramWebApp = window.Telegram.WebApp;
+
+    // Получаем информацию о пользователе
+    const user = telegramWebApp.initDataUnsafe.user;
+    console.log(user); // Для проверки, вы можете увидеть информацию в консоли
+
+    // Дополнительные действия с полученными данными пользователя
+
+    if (telegramWebApp.initData) {
+      // Отправляем данные на сервер для авторизации
+      fetch('/auth/telegram', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: telegramWebApp.initDataUnsafe.user,
+          authData: telegramWebApp.initData,
+        }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          // Успешный вход, редирект на Dashboard или Home
+          window.location.href = '/dashboard';
+        } else {
+          // Обработка ошибки
+          console.error(data.message);
+        }
+      });
+    }
+  }, []);
+
+  return <div>Loading...</div>;
 };
 
 export default Login;
